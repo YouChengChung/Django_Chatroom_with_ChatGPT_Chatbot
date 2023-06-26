537ca5f8423ef150fdd0415c736e946ad07bbc31
YuchengQB <78039363+YuchengQB@users.noreply.github.com>
2023-06-26 22:20:39 +0800

    views/urls/templates

--- chatgpt/urls.py
+++ chatgpt/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+"""
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("Home Page")
+
+def room(request):
+    return HttpResponse("Room")
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', include('base.urls')),
 ]
