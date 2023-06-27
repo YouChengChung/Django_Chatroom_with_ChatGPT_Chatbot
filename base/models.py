c67a1e11a947486593c5adacc332289c28f3250d
YuchengQB <78039363+YuchengQB@users.noreply.github.com>
2023-06-27 20:20:21 +0800

    add new table_"Topic"&"Message"

--- base/models.py
+++ base/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
 # Create your models here. #create db tables
 
-class Room(models.Model):
-    #host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    #topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model): #Room 是 Topic 的child
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True) #Class topic在上面，所以直接用，如果是在下面，要以字串'Topic'呼叫
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants = models.ManyToManyField(User, related_name='participants', blank=True)
@@ -16,3 +23,16 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+class Message(models.Model):                                    #user 是one-to-many:一個使用者對多個留言
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #django內建user model，直接import就可以用
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)#Room 是parent table，on_delete:當parent table被刪除時，要怎麼做
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
