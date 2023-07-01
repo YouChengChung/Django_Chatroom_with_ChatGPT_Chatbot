1c443d207bf4a7182d42fead776067d86d5e1260
YuchengQB <78039363+YuchengQB@users.noreply.github.com>
2023-07-01 15:13:51 +0800

    mobile_reponsiveness

--- base/views.py
+++ base/views.py
@@ -69,12 +69,13 @@ def registerPage(request):
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q)| #filter parameter符合的topic，icontains使指打部分也能篩選
         Q(name__icontains=q)
         #Q(host__icontains=q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     rooms_count  = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
 
@@ -205,3 +206,18 @@ def updateUser(request):
 
     context = {'form':form}
     return render(request,'base/update-user.html',context)
+
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics':topics}
+    return render(request,'base/topics.html', context)
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+
+    context={'room_messages':room_messages}
+    return render(request,'base/activity.html',context)
